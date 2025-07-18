579bd219fb88ed31433bbf2e809ac23735d1d357
rc <rc.devs9@gmail.com>
2025-07-18 07:46:58 -0500

    validate using inclusion instead of presence (:read is a boolean)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
-  validates :read, presence: true
+  validates :read, inclusion: { in: [ true, false ] }
 end
