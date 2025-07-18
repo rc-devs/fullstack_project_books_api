3af08936d3653244881f2522bc43f65f1d09a078
rc <rc.devs9@gmail.com>
2025-07-18 18:54:25 -0500

    set expected structure

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
+  let(:expected_book_structure) do
+    {
+      "id" => Integer,
+      "author" => String,
+      "title" => String,
+      "read" => [ Trueclass, FalseClass ]
+    }
+  end
+
   describe "GET /index" do
   # (index, show, create, update, destroy)
     # - Checking for correct JSON structure
