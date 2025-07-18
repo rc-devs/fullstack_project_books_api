6c46c5fa994800d81278a4860f2ec19f9436a7db
rc <rc.devs9@gmail.com>
2025-07-18 07:50:57 -0500

    remove boilerplate, comment test requirements

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # (index, show, create, update, destroy)
+    # - Checking for correct JSON structure
+    # - Status codes
+    # - Count changes on create/destroy
+    # - Attribute updates on update
   end
 end
