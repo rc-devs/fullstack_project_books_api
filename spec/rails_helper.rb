dee6d5e16c9bf8720197ee232bf88b041f61966c
rc <rc.devs9@gmail.com>
2025-07-18 07:19:33 -0500

    require factory bot; first test werkin

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require "faker"
+require "factory_bot_rails"
   RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
   end
