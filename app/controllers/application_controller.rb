8adf0be416d6244726a19f1411b6712304eb79d6
rc <rc.devs9@gmail.com>
2025-07-19 13:50:27 -0500

    actions in wrong controller 😱 fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
 class ApplicationController < ActionController::API
-before_action :set_books, only: [ :show ]
-
-  def index
-    books = Book.all
-    render json: books, status: :ok
-  end
-
-  def show
-    render json: @book, status: :ok
-  end
-
-  private
-  def set_books
-    @book = Book.find(params[:id])
-  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,17 @@
 class BooksController < ApplicationController
+  before_action :set_books, only: [ :show ]
+
+  def index
+    books = Book.all
+    render json: books, status: :ok
+  end
+
+  def show
+    render json: @book, status: :ok
+  end
+
+  private
+  def set_books
+    @book = Book.find(params[:id])
+  end
 end
