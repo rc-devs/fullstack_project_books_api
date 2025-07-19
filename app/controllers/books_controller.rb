88ec716b6741679eefb7fc4cb9361ef9311e3a45
rc <rc.devs9@gmail.com>
2025-07-19 14:09:43 -0500

    add controllers for create, update, destroy. add book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,47 @@
 class BooksController < ApplicationController
-  before_action :set_books, only: [ :show ]
+  before_action :set_books, only: [ :show, :update, :destroy ]
 
   def index
     books = Book.all
-    render json: books, status: :ok
+   render json: BookBlueprint.render(book, view: :normal), status: :ok
   end
 
   def show
-    render json: @book, status: :ok
+   render json: BookBlueprint.render(book, view: :normal), status: :ok
+  end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: BookBlueprint.render(book, view: :normal), status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      render json: BookBlueprint.render(book, view: :normal), status: :ok
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @book.destroy
+      render json: nil, status: :ok
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
   end
 
   private
   def set_books
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.permit(:title, :author, :read)
   end
 end
