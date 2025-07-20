472fe6b514ad75b38d52af9f02d2d5710667f842
rc <rc.devs9@gmail.com>
2025-07-19 20:27:21 -0500

    error chasing; @s or plurals mostly

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,28 @@
 class BooksController < ApplicationController
-  before_action :set_books, only: [ :show, :update, :destroy ]
+  before_action :set_book, only: [ :show, :update, :destroy ]
 
   def index
     books = Book.all
-   render json: BookBlueprint.render(book, view: :normal), status: :ok
+   render json: BookBlueprint.render(books, view: :normal), status: :ok
   end
 
   def show
-   render json: BookBlueprint.render(book, view: :normal), status: :ok
+   render json: BookBlueprint.render(@book, view: :normal), status: :ok
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
 
-    if book.save
-      render json: BookBlueprint.render(book, view: :normal), status: :created
+    if @book.save
+      render json: BookBlueprint.render(@book, view: :normal), status: :created
     else
-      render json: book.errors, status: :unprocessable_entity
+      render json: { errors: @book.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def update
     if @book.update(book_params)
-      render json: BookBlueprint.render(book, view: :normal), status: :ok
+      render json: BookBlueprint.render(@book, view: :normal), status: :ok
     else
       render json: @book.errors, status: :unprocessable_entity
     end
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
   end
 
   private
-  def set_books
+  def set_book
     @book = Book.find(params[:id])
   end
 
