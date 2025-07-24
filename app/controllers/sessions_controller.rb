0215f0390e98068afa2267ae7514e36457205b79
rc <rc.devs9@gmail.com>
2025-07-23 21:21:00 -0500

    set and utilized token variable in logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
-      render json: { token: '123' }
+      token = jwt_encode(user_id: user.id)
+      render json: { token: token }, status: :ok
     else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
   end
 
