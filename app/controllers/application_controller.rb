5b7691f3f9ecfda10d6d065df3e7eec1094d55b6
rc <rc.devs9@gmail.com>
2025-07-23 21:26:40 -0500

    decode token in app controller since all controllers inherit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split("").last if header
+  end
 end
