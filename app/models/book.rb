0eb575f2462451782f42f73bac9601580ffa1ce8
rc <rc.devs9@gmail.com>
2025-07-25 19:20:33 -0500

    validation comment, new association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
+  # validations
   validates :title, presence: true
   validates :author, presence: true
   validates :read, inclusion: { in: [ true, false ] }
+
+  # associations
+  belongs_to :user
 end
