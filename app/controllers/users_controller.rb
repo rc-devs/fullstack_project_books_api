3d4a40a7c308132649ad23f32ecdf5f578787c1d
rc <rc.devs9@gmail.com>
2025-07-27 14:45:32 -0500

    status ok, not created_at

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: :created_at
+      render json: user, status: :ok
     else
       render json: user.errors, status: :unprocessable_entity
     end
