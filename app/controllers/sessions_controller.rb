04b78d45fabeba9172d4d2426c4c6c257cdc7ea3
rc <rc.devs9@gmail.com>
2025-07-27 13:22:02 -0500

    remove extra exp

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
-      token = jwt_encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.secret_key_base)
+      token = jwt_encode({ user_id: user.id })
       render json: { token: token }, status: :ok
     else
       render json: { error: "Unauthorized" }, status: :unauthorized
