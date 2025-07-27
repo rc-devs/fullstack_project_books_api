b03d4a6dcbb05a552b2a88f350f6455fc54a7dda
rc <rc.devs9@gmail.com>
2025-07-27 13:42:54 -0500

    create user logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created_at
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
   end
 
   private
