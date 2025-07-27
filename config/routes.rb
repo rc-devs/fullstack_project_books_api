6d822c891c5c9a9aa3eed14ef0d39bdb977aeadd
rc <rc.devs9@gmail.com>
2025-07-27 14:53:02 -0500

    path for user's books, minor syntax

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  post '/login', to: 'sessions#create'
-  resources :users, only: [:create, :show]
+  post "/login", to: "sessions#create"
+  resources :users, only: [ :create, :show ]
   resources :books
-  
+
+  get "/my_books", to: "book-list#my_books"
   # Defines the root path route ("/")
   # root "posts#index"
 end
