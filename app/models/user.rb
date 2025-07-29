80c08396017df99f29de5194a6b927eba44adf92
rc <rc.devs9@gmail.com>
2025-07-28 19:19:10 -0500

    min-max length for username, min length for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_secure_password # for password hash and stuff
 
   # validations
-    validates :username, presence: true, uniqueness: :true
-    validates :password, length: { in: 5..15 }
-
+    validates :username, presence: true, uniqueness: :true, length: { in: 5..15 }
+    validates :password, length: { minimum: 8 } 
   # associations
    has_many :books
 end
