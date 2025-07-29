274d956db6088bd0a548b89e1ba7c39aa5af0d4b
rc <rc.devs9@gmail.com>
2025-07-28 19:10:21 -0500

    uniqueness true for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password # for password hash and stuff
 
   # validations
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: :true
 
   # associations
    has_many :books
