471c580ad8c7351e5b4b98ce58e88fe45d3f8c25
rc <rc.devs9@gmail.com>
2025-08-04 19:50:53 -0500

    local host restore

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "https://fe-books-oq9d1cp0j-rc-devs-projects.vercel.app/"
+      origins "https://fe-books-oq9d1cp0j-rc-devs-projects.vercel.app", "http://localhost:4200"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
