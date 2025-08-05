db9c294fccbbb24241bbeb1ce652bf4f25f16bef
rc <rc.devs9@gmail.com>
2025-08-04 19:49:29 -0500

    deployed fe url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:4200", "http://127.0.0.1:4200"
+      origins "https://fe-books-oq9d1cp0j-rc-devs-projects.vercel.app/"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
