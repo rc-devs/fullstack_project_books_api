cc2c8c31951624d401af54d811fa8ac1d5182c50
rc <rc.devs9@gmail.com>
2025-08-07 18:41:55 -0500

    set cover_image_url in fields for api responses

--- app/blueprints/book_blueprint.rb
+++ app/blueprints/book_blueprint.rb
@@ -4,6 +4,6 @@ class BookBlueprint < Blueprinter::Base
   identifier :id
 
   view :normal do
-    fields :author, :title, :read, :created_at, :updated_at
+    fields :author, :title, :read, :created_at, :updated_at, :cover_image_url
   end
 end
