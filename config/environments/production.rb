7520d82193588388113dc181627520540a23b205
rc <rc.devs9@gmail.com>
2025-08-07 19:25:02 -0500

    set host default url (hosted website on render)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options[:host] = "https://beta-blogs-evening-zzzl.onrender.com" # the url is messed up, it is the books app
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
