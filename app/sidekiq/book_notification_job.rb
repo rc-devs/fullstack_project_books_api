dbe9281ac592efbea1007415a234bac220b4172e
rc <rc.devs9@gmail.com>
2025-08-18 18:33:33 -0500

    book notification logic from exercise

--- app/sidekiq/book_notification_job.rb
+++ app/sidekiq/book_notification_job.rb
@@ -1,7 +1,9 @@
 class BookNotificationJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # Do something
+  def perform(book_id)
+    book = Book.find(book_id)
+    # Logic to send notification (e.g., email or push notification)
+    puts "Notification sent for book: #{book.title} by #{book.author}"
   end
 end
