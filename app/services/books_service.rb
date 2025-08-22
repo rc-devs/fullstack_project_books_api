1f32dc011723b9799172f981b58b17e37af4df68
rc <rc.devs9@gmail.com>
2025-08-21 19:18:15 -0500

    comments

--- app/services/books_service.rb
+++ app/services/books_service.rb
@@ -1,12 +1,12 @@
 class BookService
   module Base
-    def self.create_book(user, params)
-      book = user.books.new(params)
+    def self.create_book(user, params) # pass user and book_params (params)
+      book = user.books.new(params) # new book added
 
       if book.save
-        book 
+        book # 'returns' book where it is invoked in controller
       else
-        book.errors 
+        book.errors # errors
       end
     end
   end
